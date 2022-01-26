e7f3cad461b9ae49f9b7d17626eca16dea0b9dfc
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2022-01-26 20:14:33 +0100

    Updates

---
+++ precache-manifest.9970cf479c4e3e256cf5d4adcc518e92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc60dc943247022430f6312cc9110af0",
+    "url": "/coodles-website/index.html"
+  },
+  {
+    "revision": "758344f4c62948c378fd",
+    "url": "/coodles-website/static/css/main.89ca6767.chunk.css"
+  },
+  {
+    "revision": "1b4d93eb75fe1aa01c46",
+    "url": "/coodles-website/static/js/2.872fc2bb.chunk.js"
+  },
+  {
+    "revision": "758344f4c62948c378fd",
+    "url": "/coodles-website/static/js/main.5e99a38b.chunk.js"
+  },
+  {
+    "revision": "04c30de74aea25f196e9",
+    "url": "/coodles-website/static/js/runtime-main.4bfdedae.js"
+  }
+]);
